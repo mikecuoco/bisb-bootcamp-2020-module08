a63459a07e0ffb1a4204de9462e13324170e1860
Mike Cuoco <mcuoco12@gmail.com>
2020-09-25 11:27:43 -0700

    MSC make count_substring case insensitive

--- bootcamp_module08/core/tests/test_student_01.py
+++ bootcamp_module08/core/tests/test_student_01.py
@@ -26,3 +26,12 @@ def test_count_substring_none():
     expected_count = 0
     observed_count = count_substring(test_string, test_substring)
     assert expected_count == observed_count
+
+
+def test_count_substring_case_insensitive():
+    test_string = "AgtCcCcTaga"
+    test_substring = "aGt"
+
+    expected_count = 1
+    observed_count = count_substring(test_string, test_substring)
+    assert expected_count == observed_count
